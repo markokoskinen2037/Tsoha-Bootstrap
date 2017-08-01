12938ca9da7651aabdf49db4cc260cee44233a2c
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-01 17:48:50 +0300

    tehty esittelysivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,12 +4,27 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  //View::make('home.html');
-        echo 'Tämä on etusivu!';
+   	  View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
       View::make('helloworld.html');
+    }
+    
+    public static function esittely(){
+        View::make("suunnitelmat/esittely.html");
+    }
+    
+        public static function kirjautuminen(){
+        View::make("suunnitelmat/kirjautuminen.html");
+    }
+    
+        public static function listaus(){
+        View::make("suunnitelmat/listaus.html");
+    }
+    
+        public static function muokkaus(){
+        View::make("suunnitelmat/esittely.html");
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+    $routes->get('/esittely', function() {
+    HelloWorldController::esittely();
+  });
+  
+      $routes->get('/kirjautuminen', function() {
+    HelloWorldController::kirjautuminen();
+  });
+  
+      $routes->get('/listaus', function() {
+    HelloWorldController::listaus();
+  });
+  
+      $routes->get('/muokkaus', function() {
+    HelloWorldController::muokkaus();
+  });
