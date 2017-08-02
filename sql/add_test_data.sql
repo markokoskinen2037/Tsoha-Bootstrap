a08c01d4fedf3b911d6649cb723c3923d79cf823
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-02 16:41:42 +0300

    lisätty testidata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,17 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+  
+INSERT INTO Kayttaja (kirjautumisnimi,salasana) VALUES ("admin","1234");
+INSERT INTO Kayttaja (kirjautumisnimi,salasana) VALUES ("Pekka","salasana");
+
+INSERT INTO Luokka (luokkanimi) VALUES ("Kotityöt");
+INSERT INTO Luokka (luokkanimi) VALUES ("Autoprojekti");
+
+INSERT INTO Tarkeysaste (numeroarvo) VALUES (1);
+INSERT INTO Tarkeysaste (numeroarvo) VALUES (2);
+INSERT INTO Tarkeysaste (numeroarvo) VALUES (3);
+INSERT INTO Tarkeysaste (numeroarvo) VALUES (4);
+INSERT INTO Tarkeysaste (numeroarvo) VALUES (5);
+
+INSERT INTO Tehtava (tehtavanimi,kuvaus,luomisaika,luokkatunnus,tarkeysaste,tekija) VALUES ("Pese tiskit","Ota harja käteen ja ala hommiin jne :D",Now(),1,1,1);
+INSERT INTO Tehtava (tehtavanimi,kuvaus,luomisaika,luokkatunnus,tarkeysaste,tekija) VALUES ("Vaihda takaiskarit","Hae eka kauppiselta uudet",Now(),2,5,1);
