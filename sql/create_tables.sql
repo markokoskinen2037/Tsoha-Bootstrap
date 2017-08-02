e429d01cd58a437f02bd1328492271c7ca380f46
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-02 16:25:47 +0300

    lisätty create_table lauseet

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,28 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY,
+  kirjautumisnimi varchar(50) NOT NULL,
+  salasana varchar(50) NOT NULL,
+  admin boolean DEFAULT FALSE
+  );
+  
+CREATE TABLE Luokka(
+  id SERIAL PRIMARY KEY,
+  luokkanimi varchar(100) NOT NULL
+  );
+  
+CREATE TABLE Tarkeysaste(
+  id SERIAL PRIMARY KEY,
+  numeroarvo integer NOT NULL
+  );
+  
+CREATE TABLE Tehtava(
+  id SERIAL PRIMARY KEY,
+  tehtavanimi varchar(100) NOT NULL,
+  kuvaus varchar(500),
+  tehty boolean DEFAULT FALSE,
+  luomisaika timestamp,
+  luokkatunnus INTEGER REFERENCES Luokka(id),
+  tarkeysaste INTEGER REFERENCES Tarkeysaste(id),
+  tekija INTEGER REFERENCES Kayttaja(id)
+  );
