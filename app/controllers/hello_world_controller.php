3dd524219f76da8e3dac3ca8e13e83bf76a5acbb
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-07 17:47:02 +0300

    lisätty tehtävämalli ja testattu Kint:tiä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,30 +1,38 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+require "app/models/tehtava.php";
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('suunnitelmat/etusivu.html');
+class HelloWorldController extends BaseController {
+
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('suunnitelmat/etusivu.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      View::make('helloworld.html');
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        $tehtava = Tehtava::find(1);
+        $tehtavat = Tehtava::all();
+        
+        Kint::dump($tehtavat);
+        Kint::dump($tehtava);
+        View::make('helloworld.html');
     }
-    
-    public static function esittely(){
+
+    public static function esittely() {
         View::make("suunnitelmat/esittely.html");
     }
-    
-        public static function kirjautuminen(){
+
+    public static function kirjautuminen() {
         View::make("suunnitelmat/kirjautuminen.html");
     }
-    
-        public static function listaus(){
+
+    public static function listaus() {
         View::make("suunnitelmat/listaus.html");
     }
-    
-        public static function muokkaus(){
+
+    public static function muokkaus() {
         View::make("suunnitelmat/muokkaus.html");
     }
-  }
+
+}
---
+++ app/models/tehtava.php
@@ -0,0 +1,48 @@
+<?php
+
+class Tehtava extends BaseModel {
+
+    public $id, $tehtavanimi, $kuvaus, $tehty, $luomisaika;
+
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
+
+    public static function all() {
+        $query = DB::connection()->prepare("SELECT * FROM Tehtava");
+        $query->execute();
+        $rows = $query->fetchAll();
+        $tasks = array();
+
+        foreach ($tasks as $row) {
+            $tasks[] = new Tehtava(array(
+                "id" => $row["id"],
+                "tehtanimi" => $row["tehtavanimi"],
+                "kuvaus" => $row["kuvaus"],
+                "tehty" => $row["tehty"],
+                "luomisaika" => $row["luomisaika"],
+            ));
+        }
+
+        return $tasks;
+    }
+
+    public static function find($id) {
+        $query = DB::connection()->prepare("SELECT * FROM Tehtava WHERE id = :id LIMIT 1");
+        $query->execute(array("id" => $id));
+        $row = $query->fetch();
+
+        if ($row) {
+            $tehtava = new Tehtava(array(
+                "id" => $row["id"],
+                "tehtanimi" => $row["tehtavanimi"],
+                "kuvaus" => $row["kuvaus"],
+                "tehty" => $row["tehty"],
+                "luomisaika" => $row["luomisaika"]
+            ));
+            return $game;
+        }
+        return null;
+    }
+
+}
