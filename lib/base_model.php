11355b8c85f113d2769d09b0c77c704fd8fd58d7
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-13 17:09:56 +0300

    validointi testausta

--- lib/base_model.php
+++ lib/base_model.php
@@ -1,51 +1,51 @@
 <?php
 
-  class BaseModel{
+class BaseModel {
+
     // "protected"-attribuutti on käytössä vain luokan ja sen perivien luokkien sisällä
     protected $validators;
 
-    public function __construct($attributes = null){
-      // Käydään assosiaatiolistan avaimet läpi
-      foreach($attributes as $attribute => $value){
-        // Jos avaimen niminen attribuutti on olemassa...
-        if(property_exists($this, $attribute)){
-          // ... lisätään avaimen nimiseen attribuuttin siihen liittyvä arvo
-          $this->{$attribute} = $value;
+    public function __construct($attributes = null) {
+        // Käydään assosiaatiolistan avaimet läpi
+        foreach ($attributes as $attribute => $value) {
+            // Jos avaimen niminen attribuutti on olemassa...
+            if (property_exists($this, $attribute)) {
+                // ... lisätään avaimen nimiseen attribuuttin siihen liittyvä arvo
+                $this->{$attribute} = $value;
+            }
         }
-      }
     }
 
-    public function errors(){
-      // Lisätään $errors muuttujaan kaikki virheilmoitukset taulukkona
-      $errors = array();
+    public function errors() {
+        // Lisätään $errors muuttujaan kaikki virheilmoitukset taulukkona
+        $errors = array();
 
-      foreach($this->validators as $validator){
-          $methodToRun = $validator;
-
-          $errors = array_merge($errors, $this->{$methodToRun}());
-          
-          
-        // Kutsu validointimetodia tässä ja lisää sen palauttamat virheet errors-taulukkoon
-      }
-
-      return $errors;
+        foreach ($this->validators as $validator) {
+            $methodToRun = $validator;
+            
+            
+            $validation_errors = $this->{$methodToRun}();
+            $errors = array_merge($errors, $validation_errors);
+            // Kutsu validointimetodia tässä ja lisää sen palauttamat virheet errors-taulukkoon
+        }
+        return $errors;
     }
-    
-    public function validate_string_length($string, $length){
+
+    public function validate_string_length($string, $length) {
         $errorsInThisTest = array();
-        
-        if(strlen($string) > $length || strlen($string) == 0){
+
+        if (strlen($string) > $length || strlen($string) == 0 || $string == '' || $string == null) {
             $errorsInThisTest[] = "Nimen pituus ei kelpaa.";
         }
         return $errorsInThisTest;
     }
-    
-    public function validate_int_size($int,$maxvalue){
-        if($int <= $maxvalue){
+
+    public function validate_int_size($int, $maxvalue) {
+        if ($int <= $maxvalue) {
             echo "this is fine";
         } else {
             
         }
     }
 
-  }
+}
