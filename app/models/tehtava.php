217fa5728525f18b1ad79fc429a24038a292f01a
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-15 13:09:25 +0300

    testailua

--- app/models/tehtava.php
+++ app/models/tehtava.php
@@ -78,6 +78,9 @@ class Tehtava extends BaseModel {
     }
 
     public function update($id) {
+        
+        Kint::dump($this->tehty);
+        
         $query = DB::connection()->prepare('UPDATE Tehtava SET tehtavanimi=:tehtavanimi,kuvaus=:kuvaus,luokkatunnus=:luokkatunnus,tarkeysaste=:tarkeysaste WHERE id=:id;');
         $query->execute(array('tehtavanimi' => $this->tehtavanimi, 'kuvaus' => $this->kuvaus, 'luokkatunnus' => $this->luokkatunnus, 'tarkeysaste' => $this->tarkeysaste, 'id' => $id));
     }
