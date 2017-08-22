24331a9418143a792d9e354521d964abf376d28b
markokoskinen2037 <markokoskinen2037@gmail.com>
2017-08-22 16:23:02 +0300

    testailua

--- app/controllers/tasks_controller.php
+++ app/controllers/tasks_controller.php
@@ -14,6 +14,7 @@ class TasksController extends BaseController {
 
     public static function show($id) {
         $task = Tehtava::find($id);
+        $luokat = array();
         $luokat = Luokka::getTasksClasses($task->id);
         View::make('tehtava/esittely.html', array('taskdata' => $task, 'luokat' => $luokat));
     }
